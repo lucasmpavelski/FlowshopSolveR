be38ef40e374a9b72d32f4aba7e82f9d4695e7c9
Lucas Marcondes Pavelski <lmpavelski@yahoo.com.br>
2020-03-23 09:09:16 -0300

    Taillard accelaration neighborhood test.

--- src/flowshop-solver/problems/FSP.hpp
+++ src/flowshop-solver/problems/FSP.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <eoInt.h>
-#include <eoScalarFitness.h>
+#include "paradiseo/eo/eoInt.h"
+#include "paradiseo/eo/eoScalarFitness.h"
+#include "paradiseo/mo/problems/permutation/moShiftNeighbor.h"
 
 using FSPMax = eoInt<eoMaximizingFitness>;
 using FSPMin = eoInt<eoMinimizingFitness>;
 using FSP = FSPMin;
-
+using FSPNeighbor = moShiftNeighbor<FSP>;
--- test/test-fsp.cpp
+++ test/test-fsp.cpp
@@ -147,147 +147,43 @@ void testNWEval()
   assert(solftp.fitness() == 70);
 }
 
-using ivec = std::vector<int>;
-
-struct CompiledScheduleData
-{
-
-  int no_jobs, no_machines;
-  ivec e_times, q_times, f_times;
-  ivec makespan, flowtime;
-
-  CompiledScheduleData(int no_jobs, int no_machines)
-      : no_jobs(no_jobs), no_machines(no_machines), e_times((no_jobs + 1) * (no_machines + 1)), q_times((no_jobs + 1) * (no_machines + 2)), f_times((no_jobs + 1) * (no_machines + 1)), makespan(no_jobs), flowtime(no_jobs)
-  {
-    e_(0, 0) = 0;
-    for (int i = 1; i <= no_machines; i++)
-      e_(0, i) = 0;
-    for (int i = 1; i <= no_jobs; i++)
-      e_(i, 0) = 0;
-    q_(no_jobs, no_machines) = 0;
-    for (int j = no_machines; j >= 1; j--)
-      q_(no_jobs, j) = 0;
-    for (int i = no_jobs - 1; i >= 1; i--)
-      q_(i, no_machines + 1) = 0;
-    for (int i = 0; i <= no_jobs; i++)
-      f_(i, 0) = 0;
-  };
-
-  inline int &getTime(int j, int m, ivec &v)
-  {
-    return v[m * (no_jobs + 1) + j];
-  }
-
-  inline int &e_(int j, int m)
-  {
-    return getTime(j, m, e_times);
-  }
-
-  inline int &q_(int j, int m)
-  {
-    return getTime(j, m, q_times);
-  }
-
-  inline int &f_(int j, int m)
-  {
-    return getTime(j, m, f_times);
-  }
-
-  void compile(const FSPData &fspData, const ivec &seq, int k)
-  {
-    const int seq_size = seq.size();
-    for (int i = 1; i <= seq_size - 1; i++)
-    {
-      for (int j = 1; j <= no_machines; j++)
-      {
-        e_(i, j) = std::max(e_(i, j - 1), e_(i - 1, j)) + fspData.pt(seq[i - 1], j - 1);
-      }
-    }
-    for (int i = seq_size - 1; i >= 1; i--)
-    {
-      for (int j = no_machines; j >= 1; j--)
-      {
-        q_(i, j) = std::max(q_(i, j + 1), q_(i + 1, j)) + fspData.pt(seq[i - 1], j - 1);
-      }
-    }
-    for (int i = 1; i <= seq_size; i++)
-    {
-      for (int j = 1; j <= no_machines; j++)
-      {
-        f_(i, j) = std::max(f_(i, j - 1), e_(i - 1, j)) + fspData.pt(seq[k], j - 1);
-      }
-    }
-
-    std::fill(makespan.begin(), makespan.end(), 0);
-    std::fill(flowtime.begin(), flowtime.end(), 0);
-    for (int i = 1; i <= seq_size; i++)
-    {
-      for (int j = 1; j <= no_machines; j++)
-      {
-        int c_ij = f_(i, j) + q_(i, j);
-        makespan[i - 1] = std::max(makespan[i - 1], c_ij);
-        flowtime[i - 1] = flowtime[i - 1] + c_ij;
-      }
-    }
-  }
-
-  int getMakespan(int i)
-  {
-    return makespan[i];
-  }
-
-  int getFlowtime(int i)
-  {
-    return flowtime[i];
-  }
-};
-
-void testFastNeh()
+TEST(FSPTaillardAcelleration, Evaluation)
 {
   rng.reseed(65465l);
   const int no_jobs = 4;
   const int no_machines = 3;
   FSPData fspData(no_jobs, no_machines, 8);
-  std::cout << fspData << "\n";
 
   ivec seq = {0, 1, 2, 3};
-  //const int seq_size = 3;
-  //  int seq_size = seq.size();
-  int k = 3;
-
-  CompiledScheduleData csd(no_jobs, no_machines);
-  csd.compile(fspData, seq, k);
-  for (int i = 0; i < no_jobs; i++)
-  {
-    std::cout << i << ": " << csd.makespan[i] << "\n";
-  }
+  CompiledSchedule csd(no_jobs, no_machines);
+  csd.compile(fspData, seq);
 
   PermFSPEvalFunc<FSPMin> fsp_eval{fspData};
   FSPMin sol(4);
-  sol[0] = 0;
-  sol[1] = 1;
-  sol[2] = 2;
-  sol[3] = 3;
-  fsp_eval(sol);
-  std::cout << sol << '\n';
-  sol[0] = 0;
-  sol[1] = 1;
-  sol[2] = 3;
-  sol[3] = 2;
-  fsp_eval(sol);
-  std::cout << sol << '\n';
-  sol[0] = 0;
-  sol[1] = 3;
-  sol[2] = 1;
-  sol[3] = 2;
-  fsp_eval(sol);
-  std::cout << sol << '\n';
   sol[0] = 3;
   sol[1] = 0;
   sol[2] = 1;
   sol[3] = 2;
   fsp_eval(sol);
-  std::cout << sol << '\n';
+  ASSERT_EQ(sol.fitness(), csd.makespan[0]);
+  sol[0] = 0;
+  sol[1] = 3;
+  sol[2] = 1;
+  sol[3] = 2;
+  fsp_eval(sol);
+  ASSERT_EQ(sol.fitness(), csd.makespan[1]);
+  sol[0] = 0;
+  sol[1] = 1;
+  sol[2] = 3;
+  sol[3] = 2;
+  fsp_eval(sol);
+  ASSERT_EQ(sol.fitness(), csd.makespan[2]);
+  sol[0] = 0;
+  sol[1] = 1;
+  sol[2] = 2;
+  sol[3] = 3;
+  fsp_eval(sol);
+  ASSERT_EQ(sol.fitness(), csd.makespan[3]);
 }
 
 TEST(FSPTaillardAcelleration, IndexConvert)
@@ -298,6 +194,39 @@ TEST(FSPTaillardAcelleration, IndexConvert)
     auto kp = keyToPositionPair(i, n);
     auto pos = positionPairToKey(kp.first, kp.second, n);
     ASSERT_TRUE(i == pos);
+  }
+}
+
+TEST(FSPTaillardAcelleration, NeighborhoodEval)
+{
+  rng.reseed(65465l);
+  const int no_jobs = 50;
+  const int no_machines = 10;
+  FSPData fspData(no_jobs, no_machines, 100);
+
+  FSP sol(no_jobs);
+  PermFSPEvalFunc<FSP> fullEval(fspData, Objective::MAKESPAN);
+
+  eoInitPermutation<FSP> randomInit(no_jobs);
+  randomInit(sol);
+
+  movedSolutionStat movedSolutionStat;
+  movedSolutionStat.init(sol);
+
+  FastFSPNeighborEval ne(fspData, movedSolutionStat);
+  moFullEvalByCopy<FSPNeighbor> fullNe(fullEval);
+
+  for (int i = 0; i < (no_jobs - 1) * (no_jobs - 1); i++)
+  {
+    moShiftNeighbor<FSP> neighbor;
+    neighbor.index(i);
+    ne(sol, neighbor);
+
+    moShiftNeighbor<FSP> neighborFullEval;
+    neighborFullEval.index(i);
+    fullNe(sol, neighborFullEval);
+
+    ASSERT_EQ(neighbor.fitness(), neighborFullEval.fitness());
   }
 }
 
