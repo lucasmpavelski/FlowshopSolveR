4eab0d10ee8d2266e5d17ca73c19485f8599339c
Lucas Marcondes Pavelski <lmpavelski@yahoo.com.br>
2020-09-20 22:38:02 -0300

    Adds: backwards evaluation for no-idle FSP.

--- src/flowshop-solver/problems/NIFSPEvalFunc.hpp
+++ src/flowshop-solver/problems/NIFSPEvalFunc.hpp
@@ -68,6 +68,8 @@ template <class EOT>
 class NoIdleFSPEvalFunc : public FSPEvalFunc<EOT> {
   std::vector<int> _F;
   auto F(int j, int m) -> int& { return _F[m * noJobs() + j]; }
+  std::vector<int> _E;
+  auto E(int j, int m) -> int& { return _E[m * noJobs() + j]; }
 
  public:
   using FSPEvalFunc<EOT>::noJobs;
@@ -75,34 +77,55 @@ class NoIdleFSPEvalFunc : public FSPEvalFunc<EOT> {
   using FSPEvalFunc<EOT>::fsp_data;
 
   NoIdleFSPEvalFunc(FSPData fspd, Objective ObjT = Objective::MAKESPAN)
-      : FSPEvalFunc<EOT>(std::move(fspd), ObjT), _F(noJobs() * noMachines()) {}
+      : FSPEvalFunc<EOT>(std::move(fspd), ObjT),
+        _F(noJobs() * (noMachines() - 1)),
+        _E(noJobs() * (noMachines() - 1)) {}
 
   [[nodiscard]] auto type() const -> std::string final { return "NOIDLE"; }
 
   void completionTime(const EOT& perm,
                       std::vector<int>& completionTimes) override {
     // forward pass calculation
+    // for (int k = 0; k < noMachines() - 1; k++) {
+    //   F(0, k) = fsp_data.pt(perm[0], k + 1);
+    // }
+    // for (int j = 1; j < noJobs(); j++) {
+    //   for (int k = 0; k < noMachines() - 1; k++) {
+    //     const auto p_j_k = fsp_data.pt(perm[j], k);
+    //     const auto p_j_kp1 = fsp_data.pt(perm[j], k + 1);
+    //     const auto F_jm1_k = F(j - 1, k);
+    //     F(j, k) = std::max(F_jm1_k - p_j_k, 0) + p_j_kp1;
+    //   }
+    // }
+    // int sum_f = 0;
+    // for (int k = 0; k < noMachines() - 1; k++) {
+    //   sum_f += F(noJobs() - 1, k);
+    // }
+    // completionTimes[noJobs() - 1] = sum_f + fsp_data.machineProcTime(0);
+    // for (int j = noJobs() - 2; j >= 0; j--) {
+    //   const auto p_jp1_m = fsp_data.pt(perm[j + 1], noMachines() - 1);
+    //   completionTimes[j] = completionTimes[j + 1] - p_jp1_m;
+    // }
+
+    // backward pass calculation
     for (int k = 0; k < noMachines() - 1; k++) {
-      F(0, k) = fsp_data.pt(perm[0], k + 1);
+      E(noJobs() - 1, k) = fsp_data.pt(perm[noJobs() - 1], k);
     }
-    for (int j = 1; j < noJobs(); j++) {
+    for (int j = noJobs() - 2; j >= 0; j--) {
       for (int k = 0; k < noMachines() - 1; k++) {
         const auto p_j_k = fsp_data.pt(perm[j], k);
         const auto p_j_kp1 = fsp_data.pt(perm[j], k + 1);
-        const auto F_jm1_k = F(j - 1, k);
-        F(j, k) = std::max(F_jm1_k - p_j_k, 0) + p_j_kp1;
+        const auto E_jp1_k = E(j + 1, k);
+        E(j, k) = std::max(E_jp1_k - p_j_kp1, 0) + p_j_k;
       }
     }
-    for (int j = 0; j < noJobs(); j++) {
-      int sum_f = 0;
-      for (int k = 0; k < noMachines() - 1; k++) {
-        sum_f += F(j, k);
-      }
-      completionTimes[j] = sum_f + fsp_data.machineProcTime(0);
+    int sum_f = 0;
+    for (int k = 0; k < noMachines() - 1; k++) {
+      sum_f += E(0, k);
     }
-    for (int j = noJobs() - 2; j >= 0; j--) {
-      const auto p_jp1_m = fsp_data.pt(perm[j + 1], noMachines() - 1);
-      completionTimes[j] = completionTimes[j + 1] - p_jp1_m;
+    completionTimes[0] = sum_f + fsp_data.pt(0, noMachines() - 1);
+    for (int j = 0; j < noJobs() - 1; j++) {
+      completionTimes[j + 1] = completionTimes[j] + fsp_data.pt(j + 1, noMachines() - 1);
     }
   }
 };
